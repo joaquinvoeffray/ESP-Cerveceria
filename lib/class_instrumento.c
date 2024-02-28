542fddec94d1c8879b885c9cb68170b97d699061
joaquinvoeffray <joaquinv1234@hotmail.com>
2024-02-28 15:37:11 -0300

    control de led1 por temperatura

--- lib/class_instrumento.c
+++ lib/class_instrumento.c
@@ -10,25 +10,30 @@
     void Estado(short,float,int);
     int getMedicion();
     void settMedicion();
-
  };
+
  //constructor 1
- _instrumento::_instrumento(short _Nombre, float _Medicion, int _Estado){
+ _instrumento::_instrumento(short _Nombre){
     nombre=_Nombre;
-    medicion=_Medicion;
-    estado=_Estado;
  }
+
+
 int _instrumento::getMedicion(){        //retornamos el valor encapsulado del valor medido
      return medicion;
 }
+
+
 void _instrumento::settMedicion(float _Medicion){
     medicion = _Medicion;
 }
+
+
 //funsiones 
     void _instrumento::GuardarMedicion(){ //es de clase void, pertenece a la clase instrumento y se llama guardar medicion
       //ver como guardar datos a firebase
       saveinfirebase: medicion;
     }
+
     void _instrumento::VerificacionFunsionamiento(){
       //corroborar de alguna manera que el instrumento este midiendo bien
     }
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,16 @@
 #include <class_instrumento>
 #include <class_alarma>
 #include <class_chek>
-
+#define LED1 = D1
 // put function declarations here:
 int myFunction(int, int);
+float medicion;
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(3, 4);
- 
+  pinMode (LED1,OUTPUT);
+
   }
  
 void loop() {
@@ -18,9 +20,17 @@ void loop() {
   _check(maserado,estado,0);
   _check(coccion,estado,0);
   //envio de alertas
-
-
-
+  //Constructor de instrumento
+  _instrumento::_instrumento(TemperaturaAguaInicial);
+  //encender led a x temperatura
+  medicion = getMedicion;
+  
+   if (medicion >= 30){
+    digitalWrite (LED1,HIGH);
+   }
+else
+  digitalWrite(LED1,LOW);
+  
 }
 
 // put function definitions here:
